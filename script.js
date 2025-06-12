eb4210352c708b7dc8482ea1541b2357bc2d92fb
AlanikREDAWN <lilia.dostie@socprep.org>
2025-06-12 16:22:57 -0400

    adding real content

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 function getContentInPDF() {
 
-    let fname = document.getElementById('fname').value;
+    let name = document.getElementById('name').value;
 
-    let lname = document.getElementById('lname').value;
+    // let fname = document.getElementById('fname').value;
 
-    let pass = document.getElementById('pass').value;
+    // let lname = document.getElementById('lname').value;
 
-    
+    // let pass = document.getElementById('pass').value;
+
+    // fetch("results.html").then(r=>r.text()).then((html)=>{ // get the content of products.html
+    //     let element = document.createElement("html");
+    //     element.innerHTML = html; // parse the html
+    //     p1 = element.querySelector("#body");
+    //     // let thing = element.querySelector();
+    //     });
+
     let element = document.createElement('div');
 
-    element.innerHTML = '<h1>Form Data</h1>' + '<br>' + fname + lname + pass;
+    // element.innerHTML = '<h1>Shipwrecked Survival Guide</h1>' + '<br>' + fname + lname + pass;
+    element.innerHTML = '<h1>' + name + "'s Shipwrecked Survival Guide</h1>";
+    // element.innerHTML = fetch("results.html");
+    // element.innerHTML = element.querySelector("#body")
+    // console.log(fetch("results.html"))
+    // console.log(element.querySelector("#body"))
+
+    
+
+    
+    // let formElement = document.getElementById('results');
 
     html2pdf().from(element).save();
+    // html2pdf().from(formElement).save();
+    // html2pdf().from(p1).save();
+    // html2pdf().from(thing).save();
+}
+
+function generatePDF() {
+    let formElement = document.getElementById('pdfForm');
+    html2pdf().from(formElement).save();
 }
